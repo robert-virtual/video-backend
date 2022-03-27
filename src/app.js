b45d0c1659ec9cd11f3a75baab365e9c73132a97
robert-virtual <robertocastill63@gmail.com>
2022-03-27 11:04:36 -0600

    ruta para eliminar videos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+  app.use(require("morgan")("dev"));
+}
+
 app.use(express.static("uploads"));
 app.use(express.json());
 app.use("/videos", require("./routes/videos"));
--- src/routes/videos.js
+++ src/routes/videos.js
@@ -1,8 +1,39 @@
+const fs = require("fs");
 const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
 const { upload } = require("../helpers/multer");
+const path = require("path");
 const prisma = new PrismaClient();
 const { round, random } = Math;
+// eliminar files
+router.get("/delete", async (req, res) => {
+  const { secret } = req.query;
+  if (secret != process.env.DELETE_FILES) {
+    return res.json({ msg: "Acceso denegado" });
+  }
+  fs.readdir("./uploads", (err, files) => {
+    if (err) {
+      return res.json({ err });
+    }
+    console.log(files);
+    try {
+      files.forEach((f) => {
+        fs.unlinkSync(path.resolve("./uploads/", f));
+      });
+      res.json({ msg: `${files.length} archivos borrados` });
+    } catch (error) {
+      res.json({ msg: error.message });
+    }
+  });
+});
+// obtener files
+router.get("/files", async (req, res) => {
+  fs.readdir("./uploads", (err, files) => {
+    console.log(files);
+    res.json(files);
+  });
+});
+
 // obtener videos
 router.get("/", async (req, res) => {
   let videos = await prisma.video.findMany({
